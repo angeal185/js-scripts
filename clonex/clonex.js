59c581dd3f7ad8b2c10b3c4c4b597e29d836934f
Angeal185 <beneaves01@hotmail.com>
2017-02-23 07:35:41 +0800

    Add files via upload

---
+++ clonex/clonex.js
@@ -0,0 +1,7 @@
+$.fn.clonex = function(count, cloneEvents) {
+	var tmp = [];
+	for ( var i = 0; i < count; i++ ) {
+		$.merge( tmp, this.clone( cloneEvents ).get() );
+	}
+	return this.pushStack( tmp );
+};
